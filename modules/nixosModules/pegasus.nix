0f0b4cb1b30a610ef60ce384cc810c1dbf8d2560
dasBente <dasbente@gmail.com>
2026-09-24 20:57:33 +0200

    refactor: parse attrs to ini

--- modules/nixosModules/pegasus.nix
+++ modules/nixosModules/pegasus.nix
@@ -5,30 +5,46 @@
     config,
     ...
   }: let
-    launcher = name: info: let
+    attrsToIni = attrs: let
+      toOption = opt: value: "${opt}=\"${value}\"";
+      toLevel = lvl: options:
+        lib.concatStringsSep
+        "\n"
+        (["[${lvl}]"] ++ lib.mapAttrsToList toOption options);
+    in
+      lib.concatStringsSep
+      "\n\n"
+      (lib.mapAttrsToList toLevel attrs);
+
+    toPlatform = _: info: let
       exec = "/run/current-system/sw/bin/retroarch";
       core = "/run/current-system/sw/lib/retroarch/cores/${info.core}_libretro.so";
-    in ''
-      [${name}]
-      launch="${exec} -L ${core} \"{file.path}\""
-    '';
+    in {launch = "${exec} -L ${core} \"{file.path}\"";};
 
-    core-txt =
-      lib.concatStringsSep "\n"
-      (lib.mapAttrsToList launcher config.emu-nix.enabledSystems);
+    skyscraperAttrs =
+      {
+        main = {
+          frontend = "pegasus";
+        };
+      }
+      // (lib.mapAttrs toPlatform config.emu-nix.enabledSystems);
 
-    skyscraper-config = pkgs.writeText "skyscraper-config.ini" ''
-      [main]
-      frontend="pegasus"
-
-      ${core-txt}
-    '';
+    skyscraper-config =
+      pkgs.writeText "skyscraper-config.ini" (attrsToIni skyscraperAttrs);
 
     skyscraper-sh = pkgs.writeShellScriptBin "Skyscraper" ''
       exec ${pkgs.skyscraper}/bin/Skyscraper -c ${skyscraper-config} "$@"
     '';
   in {
-    options.emu-nix.pegasus.enable = lib.mkEnableOption "Install pegasus frontend";
+    options.emu-nix.pegasus = {
+      enable = lib.mkEnableOption "Install pegasus frontend";
+
+      romPath = lib.mkOption {
+        default = "/home/<USER>/RetroPie/roms";
+        type = lib.types.string;
+        description = "String path to ROM directory.";
+      };
+    };
 
     config.environment.systemPackages = lib.mkIf config.emu-nix.pegasus.enable [
       pkgs.pegasus-frontend
